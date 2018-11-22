7e765c9651d00116ec5f603481a4a107b7c38fc3
seme <smewled2012@gmail.com>
2018-11-21 19:25:29 -0500

    update5

---
+++ src/main/java/com/example/demo/Passenger.java
@@ -0,0 +1,100 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private  String firstName;
+
+    private String lastName;
+
+    private  String dob;
+
+    private  String phonenum;
+
+    private String email;
+
+    private String tripClass;
+    private int numPassenger;
+    private String tripType;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTripClass() {
+        return tripClass;
+    }
+
+    public void setTripClass(String tripClass) {
+        this.tripClass = tripClass;
+    }
+
+    public int getNumPassenger() {
+        return numPassenger;
+    }
+
+    public void setNumPassenger(int numPassenger) {
+        this.numPassenger = numPassenger;
+    }
+
+    public String getTripType() {
+        return tripType;
+    }
+
+    public void setTripType(String tripType) {
+        this.tripType = tripType;
+    }
+}
